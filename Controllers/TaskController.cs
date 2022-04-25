06cb21015cfd0ebb3b7ff0ddaa381e4263ec7b4c
heyitztoby <54342829+heyitztoby@users.noreply.github.com>
2022-04-25 18:08:27 +0800

    update

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
  
 namespace producer.Controllers
 {
@@ -14,52 +15,49 @@ namespace producer.Controllers
     [Route("[controller]")]
     public class TaskController : ControllerBase
     {
+
+        private static readonly HttpClient client = new HttpClient();
  
         [HttpPost]
-        public void Post([FromBody] Tasks taskinfo)
+        public async void Post([FromBody] Tasks taskinfo)
         {
+            var values = new Dictionary<string, string> {
+                { "email", taskinfo.Email },
+                { "password", taskinfo.Password }
+                };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync("https://reqres.in/api/login", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            responseString = responseString.Split("\"")[1];
+            
+            if (responseString.Equals("token")) {
             var factory = new ConnectionFactory() { 
-                //HostName = "localhost" , 
-                // Port = 30724
             HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST"),
             Port = Convert.ToInt32(Environment.GetEnvironmentVariable("RABBITMQ_PORT"))
             };
-
-            // factory.Uri = new Uri("https://reqres.in/api/login");
- 
             
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                // Console.WriteLine(connection.res);
                 channel.QueueDeclare(queue: "TaskQueue",
                                      durable: false,
                                      exclusive: false,
                                      autoDelete: false,
                                      arguments: null);
- 
-                string email = taskinfo.Email;
-                string password = taskinfo.Password;
                 string task = taskinfo.Task;
-                var emailBody = Encoding.UTF8.GetBytes(email);
-                var passwordBody = Encoding.UTF8.GetBytes(password);
                 var taskBody = Encoding.UTF8.GetBytes(task);
-                
- 
-                channel.BasicPublish(exchange: "",
-                                     routingKey: "Task",
-                                     basicProperties: null,
-                                     body: emailBody);
 
                 channel.BasicPublish(exchange: "",
-                                     routingKey: "Task",
-                                     basicProperties: null,
-                                     body: passwordBody);
-
-                channel.BasicPublish(exchange: "",
-                                     routingKey: "Task",
+                                     routingKey: "TaskQueue",
                                      basicProperties: null,
                                      body: taskBody);
+                }
+            } else {
+                Console.WriteLine("ERROR 401");
             }
         }
     }
